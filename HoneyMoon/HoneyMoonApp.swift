fb3dac88ccb2b3b514156089f2aab4f7e6d37afc
wnifirami <rami.ounifi@esprit.tn>
2022-08-26 13:02:19 +0100

    creating the project

---
+++ HoneyMoon/HoneyMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyMoonApp.swift
+//  HoneyMoon
+//
+//  Created by Rami Ounifi on 26.08.22.
+//
+
+import SwiftUI
+
+@main
+struct HoneyMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
